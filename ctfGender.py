1c42a514d89836dbba3af6848d1a4c5a8b926f47
Joe Jeskiewicz <artexercise@yahoo.com>
2016-10-28 23:33:46 -0400

    Making the Attribute better

--- ctfGender.py
+++ ctfGender.py
@@ -1,15 +1,26 @@
 __author__ = "jjeskiewicz"
 
 class ctfGender:
-   Male = "Male"
-   Female = "Female"
-   Unknown = "Unknown"
-   Hermaphrodite = "Hermaphrodite"
-   Androgynous = "Androgynous"
+   class objGender:
+      def __init__(self, name, desc):
+         self.name = name
+         self.desc = desc
+      def __str__(self):
+         return self.name
+
+   Male = objGender("Male", "It's a guy.")
+   Female = objGender("Female", "It's a gal.")
+   Unknown = objGender("Unknown", "I don't know for... reasons.")
+   Hermaphrodite = objGender("Hermaphrodite", "Little bit man, little bit woman.")
+   Androgynous = objGender("Androgynous", "Ummm. Neither guy nor gal?")
 
    AttributeList = [Male, Female, Unknown, Hermaphrodite, Androgynous]
+
 
 if __name__ == "__main__":
    import random
    for i in range(5):
-      print(random.choice(ctfGender.AttributeList))
+      g = random.choice(ctfGender.AttributeList)
+      print("{0} - {1}".format(g, g.desc))
+   print("Calling Directly from class like an ENUM: {0}".format(ctfGender.Male))
+   print("Because Gender is also an Object with another attribute: {0}".format(ctfGender.Male.desc))
