7836c3258bb45c5a0a4777845bd04d138061827e
Joe Jeskiewicz <artexercise@yahoo.com>
2016-10-31 23:32:41 -0400

    First Names

---
+++ ctfGivenName.py
@@ -0,0 +1,306 @@
+__author__ = "jjeskiewicz"
+
+class ctfGivenName:
+   class objGivenName:
+      def __init__(self, name):
+         self.name = name
+      def __str__(self):
+         return self.name
+
+   John = objGivenName("John")
+   William = objGivenName("William")
+   James = objGivenName("James")
+   George = objGivenName("George")
+   Charles = objGivenName("Charles")
+   Robert = objGivenName("Robert")
+   Joseph = objGivenName("Joseph")
+   Frank = objGivenName("Frank")
+   Edward = objGivenName("Edward")
+   Thomas = objGivenName("Thomas")
+   Paul = objGivenName("Paul")
+   Ernest = objGivenName("Ernest")
+   Andrew = objGivenName("Andrew")
+   Leonard = objGivenName("Leonard")
+   Leroy = objGivenName("Leroy")
+   Eddie = objGivenName("Eddie")
+   Harvey = objGivenName("Harvey")
+   Everett = objGivenName("Everett")
+   Patrick = objGivenName("Patrick")
+   Sylvester = objGivenName("Sylvester")
+   Juan = objGivenName("Juan")
+
+   AttributeList = []
+   AttributeList1900sMale = [John]
+
+if __name__ == "__main__":
+   import random
+   for i in range(5):
+      g = random.choice(ctfSurname.AttributeList)
+      print("{0}".format(g))
+   print("Calling Directly from class like an ENUM: {0}".format(ctfSurname.Smith))
+
+  1900 - 1909
+Rank	Male
+name	Female
+name
+1	John	Mary
+2	William	Helen
+3	James	Margaret
+4	George	Anna
+5	Charles	Ruth
+6	Robert	Elizabeth
+7	Joseph	Dorothy
+8	Frank	Marie
+9	Edward	Florence
+10	Thomas	Mildred
+20	Paul	Bertha
+30	Ernest	Martha
+40	Andrew	Eva
+50	Leonard	Lillie
+60	Leroy	Lena
+70	Eddie	Cora
+80	Harvey	Ada
+90	Everett	Daisy
+100	Patrick	Velma
+150	Sylvester	Rachel
+200	Juan	Eliza
+
+1910's
+Rank	Name	Number	Name	Number
+1	John	376,322	Mary	478,643
+2	William	303,026	Helen	248,155
+3	James	275,077	Dorothy	207,478
+4	Robert	239,190	Margaret	189,233
+5	Joseph	179,300	Ruth	173,680
+6	George	176,614	Mildred	123,996
+7	Charles	173,540	Anna	119,000
+8	Edward	125,722	Elizabeth	116,810
+9	Frank	116,019	Frances	105,600
+10	Thomas	91,619	Virginia	94,321
+20	Donald	57,004	Catherine	65,821
+30	Kenneth	43,250	Edith	56,058
+40	Samuel	34,167	Clara	44,346
+50	Peter	24,937	Sarah	38,118
+60	Floyd	21,472	Nellie	30,878
+70	Marvin	17,821	Minnie	27,486
+80	Cecil	15,986	Lucy	23,116
+90	Harvey	13,304	Wilma	21,383
+100	Homer	11,471	Maxine	17,800
+150	Matthew	6,228	Wanda	10,735
+200	Bert	4,327	Lela	7,007
+
+1920's
+Rank	Name	Number	Name	Number
+1	Robert	576,350	Mary	701,736
+2	John	564,062	Dorothy	368,858
+3	James	515,305	Helen	290,396
+4	William	512,386	Betty	283,084
+5	Charles	298,025	Margaret	245,003
+6	George	261,946	Ruth	218,504
+7	Joseph	243,807	Virginia	169,550
+8	Richard	222,604	Doris	151,184
+9	Edward	197,353	Mildred	147,155
+10	Donald	191,819	Frances	140,384
+20	Arthur	97,113	Marjorie	91,220
+30	Clarence	66,577	Patricia	74,740
+40	Alfred	50,159	Gloria	62,854
+50	Marvin	39,586	Lorraine	49,937
+60	Floyd	33,152	Joan	45,352
+70	Philip	27,269	Bertha	38,571
+80	Calvin	24,782	Elaine	34,906
+90	Victor	21,498	Myrtle	30,567
+100	Oscar	17,739	Ellen	28,389
+150	Earnest	10,868	Hattie	16,370
+200	Forrest	6,909	Jeannette	9,992
+
+1930's
+Rank	Name	Number	Name	Number
+1	Robert	590,668	Mary	572,906
+2	James	547,346	Betty	300,361
+3	John	487,823	Barbara	296,386
+4	William	416,616	Shirley	229,354
+5	Richard	330,416	Patricia	220,564
+6	Charles	303,125	Dorothy	210,380
+7	Donald	283,620	Joan	173,419
+8	George	192,237	Margaret	157,351
+9	Thomas	183,845	Nancy	142,473
+10	Joseph	180,523	Helen	140,402
+20	Billy	88,306	Beverly	80,838
+30	Larry	65,228	Evelyn	69,349
+40	Norman	49,979	Judith	49,900
+50	Marvin	41,073	Wanda	43,067
+60	Alfred	34,371	Sarah	36,504
+70	Franklin	26,379	Sally	34,675
+80	Glenn	24,219	Ethel	31,583
+90	Eddie	22,319	Jo	28,652
+100	Charlie	19,150	Julia	24,935
+150	Dick	10,161	Leona	14,172
+200	Alex	6,304	Daisy	9,060
+
+1940's
+Rank	Name	Number	Name	Number
+1	James	795,647	Mary	639,999
+2	Robert	758,005	Linda	531,615
+3	John	711,526	Barbara	425,230
+4	William	556,384	Patricia	411,389
+5	Richard	477,385	Carol	292,316
+6	David	426,294	Sandra	265,525
+7	Charles	361,671	Nancy	252,030
+8	Thomas	350,759	Sharon	221,218
+9	Michael	336,559	Judith	218,007
+10	Ronald	282,398	Susan	212,011
+20	Dennis	162,084	Dorothy	132,237
+30	Douglas	80,755	Martha	95,375
+40	Joe	71,993	Jane	72,529
+50	Johnny	57,179	Connie	56,595
+60	Danny	46,650	Jo	48,447
+70	Francis	39,523	Suzanne	41,403
+80	Don	36,240	Louise	38,651
+90	Alfred	31,989	Marcia	34,394
+100	Keith	25,871	Dianne	32,291
+150	Chester	15,280	Eva	17,563
+200	Fredrick	8,228	Stella	11,359
+
+1950's
+Rank	Name	Number	Name	Number
+1	James	843,377	Mary	625,532
+2	Michael	837,084	Linda	564,261
+3	Robert	830,032	Patricia	459,587
+4	John	797,528	Susan	437,688
+5	David	769,555	Deborah	430,493
+6	William	590,973	Barbara	345,686
+7	Richard	535,149	Debra	341,282
+8	Thomas	454,169	Karen	332,483
+9	Mark	382,419	Nancy	286,757
+10	Charles	360,979	Donna	270,310
+20	Stephen	207,171	Janet	167,500
+30	Anthony	136,622	Teresa	114,264
+40	Patrick	96,819	Connie	88,796
+50	Dale	75,512	Bonnie	83,002
+60	Russell	62,377	Jean	72,259
+70	Jimmy	54,857	Maria	62,392
+80	Mike	48,343	Ellen	55,375
+90	Micheal	40,933	Kimberly	48,959
+100	Vincent	35,343	Doris	42,894
+150	Vernon	20,125	Joy	24,514
+200	Kelly	13,124	Grace	15,915
+
+1960's
+Rank	Name	Number	Name	Number
+1	Michael	833,402	Lisa	496,980
+2	David	734,169	Mary	355,223
+3	John	713,653	Susan	287,661
+4	James	684,986	Karen	286,044
+5	Robert	650,975	Kimberly	259,092
+6	Mark	441,513	Patricia	231,137
+7	William	421,610	Linda	225,412
+8	Richard	374,010	Donna	213,457
+9	Thomas	327,231	Michelle	208,285
+10	Jeffrey	302,105	Cynthia	196,606
+20	Christopher	237,285	Jennifer	164,429
+30	Douglas	135,047	Kelly	128,078
+40	Randy	93,889	Kathy	101,122
+50	Roger	71,455	Michele	86,992
+60	Chris	56,376	Ann	72,093
+70	Vincent	49,501	Renee	55,811
+80	Barry	47,227	Anne	47,771
+90	Tim	41,588	Sarah	43,914
+100	Greg	36,465	Tamara	39,004
+150	Glen	21,892	Phyllis	25,697
+200	Gene	13,699	Pam	17,109
+
+1970's
+Rank	Name	Number	Name	Number
+1	Michael	707,737	Jennifer	581,791
+2	Christopher	475,669	Amy	269,009
+3	Jason	462,954	Melissa	253,291
+4	David	446,004	Michelle	249,140
+5	James	444,982	Kimberly	229,112
+6	John	402,879	Lisa	228,676
+7	Robert	397,452	Angela	225,222
+8	Brian	322,870	Heather	203,939
+9	William	283,588	Stephanie	160,470
+10	Matthew	278,010	Nicole	144,672
+20	Thomas	186,901	Laura	118,216
+30	Chad	113,602	Patricia	81,118
+40	Edward	83,519	Jamie	67,853
+50	Douglas	65,212	Sara	56,961
+60	Shane	50,295	Brenda	48,596
+70	Corey	41,027	Leslie	44,837
+80	Tony	35,691	Theresa	40,088
+90	Carl	31,394	Tamara	37,072
+100	Jimmy	27,101	Ann	32,031
+150	Manuel	17,687	Aimee	19,409
+200	Harry	11,657	Adrienne	12,796
+
+1980's
+Rank	Name	Number	Name	Number
+1	Michael	663,592	Jessica	469,439
+2	Christopher	554,793	Jennifer	440,836
+3	Matthew	458,894	Amanda	369,698
+4	Joshua	396,533	Ashley	352,147
+5	David	383,610	Sarah	272,559
+6	James	356,362	Stephanie	218,131
+7	Daniel	345,414	Melissa	217,896
+8	Robert	321,598	Nicole	210,456
+9	John	321,034	Elizabeth	198,938
+10	Joseph	299,367	Heather	191,841
+20	Anthony	211,804	Crystal	140,998
+30	Kyle	141,910	Sara	104,640
+40	Paul	104,339	April	75,942
+50	Alexander	81,943	Allison	62,323
+60	Jordan	57,140	Kristina	45,943
+70	Cory	44,349	Veronica	39,224
+80	Nathaniel	38,639	Carrie	36,083
+90	Gabriel	32,304	Jasmine	32,941
+100	Christian	30,244	Misty	29,116
+150	Mathew	18,619	Jenny	19,714
+200	Geoffrey	12,725	Taylor	13,615
+
+1990's
+Rank	Name	Number	Name	Number
+1	Michael	462,289	Jessica	303,069
+2	Christopher	360,187	Ashley	301,789
+3	Matthew	351,585	Emily	237,216
+4	Joshua	329,097	Sarah	224,253
+5	Jacob	298,279	Samantha	223,982
+6	Nicholas	275,275	Amanda	191,056
+7	Andrew	272,795	Brittany	190,810
+8	Daniel	272,154	Elizabeth	172,595
+9	Tyler	262,260	Taylor	169,012
+10	Joseph	260,627	Megan	160,353
+20	Robert	205,394	Amber	115,590
+30	Benjamin	133,867	Morgan	91,182
+40	Adam	106,869	Christina	74,638
+50	Jesse	73,764	Erin	65,326
+60	Hunter	60,512	Kelly	55,713
+70	Carlos	55,149	Caitlin	49,030
+80	Bradley	48,072	Alexandria	44,091
+90	Seth	42,199	Monica	38,884
+100	Jack	37,469	Cheyenne	31,843
+150	Omar	23,711	Hayley	21,854
+200	Larry	15,941	Mia	14,618
+
+2000's
+Rank	Name	Number	Name	Number
+1	Jacob	273,690	Emily	223,612
+2	Michael	250,420	Madison	193,081
+3	Joshua	231,808	Emma	181,174
+4	Matthew	221,443	Olivia	155,951
+5	Daniel	203,602	Hannah	155,626
+6	Christopher	203,184	Abigail	150,814
+7	Andrew	202,363	Isabella	149,436
+8	Ethan	201,701	Samantha	134,216
+9	Joseph	194,583	Elizabeth	133,305
+10	William	194,237	Ashley	132,988
+20	Noah	143,348	Chloe	96,410
+30	Gabriel	119,711	Julia	70,840
+40	Jack	95,472	Alexandra	61,216
+50	Jayden	88,473	Addison	51,357
+60	Adam	70,946	Gabriella	47,342
+70	Bryan	61,896	Sara	43,982
+80	Sebastian	53,822	Evelyn	40,704
+90	Richard	48,538	Brooklyn	36,808
+100	Tristan	43,261	Amelia	33,245
+150	Ricardo	27,172	Sabrina	22,324
+200	Martin	18,738	Jasmin	16,896
